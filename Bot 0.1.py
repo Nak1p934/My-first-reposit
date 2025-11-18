16a1556092c2e71eb39944c0651d06cad1215bb4
Nak1p934 <leonleon0308@gmail.com>
2025-11-18 13:05:02 +0300

    0.1

---
+++ Bot 0.1.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN_API = "8417220012:AAFKjrPX0pKLdrp6uj_aPOrQV9m1E8EVQqc"
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hello! I'm your friendly bot. How can I assist you today?")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
