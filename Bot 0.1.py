7500b92f3d1808a7894aa442872f7d1919cdcc28
Nak1p934 <leonleon0308@gmail.com>
2025-11-18 13:15:06 +0300

    2.24

--- Bot 0.1.py
+++ Bot 0.1.py
@@ -4,7 +4,7 @@ TOKEN_API = "8417220012:AAFKjrPX0pKLdrp6uj_aPOrQV9m1E8EVQqc"
 bot = Bot(TOKEN_API)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler()
 async def send_welcome(message: types.Message):
     await message.reply("Hello! I'm your friendly bot. How can I assist you today?")
 
