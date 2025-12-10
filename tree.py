4963a2869ede377ef97e4a5006692826f6dacc5d
Nak1p934 <leonleon0308@gmail.com>
2025-12-10 14:05:45 +0300

    colors

--- tree.py
+++ tree.py
@@ -1,13 +1,20 @@
 import random
-ctr = 1
-for i in range(5):
-    for j in range(1):
-        print(' ' * ((9 - ctr) // 2), "*" * ctr, end = "")
-    ctr += 2
-    print("\n", end = "")
-# get tree
-'''     *
+import sys
+from colorama import Fore
+tree = '''     *
     ***
    *****
   *******
- *********'''
+ *********
+    |#|'''
+colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+def lights():
+    for j in range(30):
+        print("\r")
+        for i in tree:
+            if i == "*" or i == "|" or i == "#":
+                print(random.choice(colors) + i, end = "")
+            else:
+                print(i, end = "")
+            sys.stdout.flush()
+lights()
